c101cb1538114d812e6740ef1a9324d543e23dc7
Valentin Bancarel <vabancarel@iutclinf101.iut.local>
2015-05-21 08:12:35 +0200

    Ajout de la classe véhicules

---
+++ TP2/Vehicule.h
@@ -0,0 +1,13 @@
+//
+//  Vehicule.h
+//  TP2
+//
+//  Created by Valentin Bancarel on 21/05/2015.
+//  Copyright (c) 2015 bancarelvalentin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Vehicule : NSObject
+
+@end
---
+++ TP2/Vehicule.m
@@ -0,0 +1,13 @@
+//
+//  Vehicule.m
+//  TP2
+//
+//  Created by Valentin Bancarel on 21/05/2015.
+//  Copyright (c) 2015 bancarelvalentin. All rights reserved.
+//
+
+#import "Vehicule.h"
+
+@implementation Vehicule
+
+@end
